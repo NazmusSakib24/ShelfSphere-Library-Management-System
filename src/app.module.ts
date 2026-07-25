167587bdfba52622b53afcc6a5fad4950d7fcce4
Nazmus Sakib <ahmednazmussakib629@gmail.com>
2026-07-25 23:41:39 +0600

    impleneted database (.env) and project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
 import { CategoriesModule } from './categories/categories.module';
 import { BorrowsModule } from './borrows/borrows.module';
@@ -11,18 +14,29 @@ import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password:'123456',
-      database: 'shelfsphere_db',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
 
       autoLoadEntities: true,
       synchronize: true,
     }),
-    AuthModule, UsersModule, BooksModule, CategoriesModule, BorrowsModule, ReservationsModule],
+
+    AuthModule,
+    UsersModule,
+    BooksModule,
+    CategoriesModule,
+    BorrowsModule,
+    ReservationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
