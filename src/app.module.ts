d11d3761940bac3d157659582fc6b81f27c93079
Nazmus Sakib <ahmednazmussakib629@gmail.com>
2026-07-25 17:44:18 +0600

    Configure PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password:'123456',
+      database: 'shelfsphere_db',
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
