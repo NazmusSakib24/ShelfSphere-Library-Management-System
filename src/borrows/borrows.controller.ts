e73c469d6f3fcf03ce6df17e5fa5dc6bfbd283ce
NazmusSakib24 <155712884+NazmusSakib24@users.noreply.github.com>
2026-07-28 22:34:11 +0600

    Delete src/borrows directory

--- src/borrows/borrows.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { BorrowsService } from './borrows.service';
-
-@Controller('borrows')
-export class BorrowsController {
-  constructor(private readonly borrowsService: BorrowsService) {}
-}
